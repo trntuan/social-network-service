4bfbee876f046e1d8d46508855fdf9799f2c6795
Tuan Truong <tuan.truong@widosoft.com>
2024-05-21 16:44:32 +0700

    add origin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3030',
   });
+
   app.use(cookieParser());
   await app.listen(3000);
 }
