239deea8dafbd73c2cf281db8bf1aa2228ad9513
Tuan Truong <tuan.truong@widosoft.com>
2024-05-21 16:55:59 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: 'http://localhost:3030',
     credentials: true,
   });
 
