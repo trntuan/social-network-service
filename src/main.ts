65b1f62743072346d2bf4b9ceced233857fae350
Tuan Truong <tuan.truong@widosoft.com>
2024-05-21 16:51:20 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3030',
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
   });
 
   app.use(cookieParser());
