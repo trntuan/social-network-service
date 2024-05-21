d65f188009d8fd7761d4f1649d270c420adc6fce
Tuan Truong <tuan.truong@widosoft.com>
2024-05-21 16:02:45 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(3000);
 }
